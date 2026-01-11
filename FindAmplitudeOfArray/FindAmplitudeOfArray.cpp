9f70f84a9f1e2e898f2cc4bf3225e1bc9d2225ed
Alexander <pchp250g8@tutamail.com>
2026-01-11 12:42:43 +0300

    Добавьте файлы проекта.

---
+++ FindAmplitudeOfArray/FindAmplitudeOfArray.cpp
@@ -0,0 +1,63 @@
+﻿// FindAmplitudeOfArray.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <valarray>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char* argv[])
+{
+    int nElems;
+    std::cout << "Input a count of elements in array\r\n";
+    std::cin >> nElems;
+    if (nElems == 0)
+    {
+        std::cout << "Empty arrays not allowed\r\n";
+        std::cin.ignore();
+        std::cin.get();
+        return 1;
+    }
+    std::valarray<int> iNumbers(nElems);
+    srand(time(nullptr));
+    std::cout << "Generating array by filling " << nElems << " elements\r\n";
+    for (int i = 0; i < nElems; i++)
+    {
+        iNumbers[i] = 1 + rand() % 100;
+        std::cout << iNumbers[i] << " ";
+    }
+    int iMax = iNumbers[0];
+    int nMaxIndex = 0;
+    int iMin = iNumbers[0];
+    int nMinIndex = 0;
+    for (int i = 1; i < nElems; i++)
+    {
+        if (iNumbers[i] > iMax)
+        {
+            iMax = iNumbers[i];
+            nMaxIndex = i;
+        }
+        if (iNumbers[i] < iMin)
+        {
+            iMin = iNumbers[i];
+            nMinIndex = i;
+        }
+
+    }
+    std::cout << "\r\nFound maximum element: " << iMax << " with index " << nMaxIndex << "\r\n";
+    std::cout << "Found minimum element: " << iMin << " with index " << nMinIndex << "\r\n";
+    std::cout << "Found amplitude:" << iMax - iMin << "\r\n";
+    std::cin.ignore();
+    std::cin.get();
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
